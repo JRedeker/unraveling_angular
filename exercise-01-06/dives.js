527f2a4e8eed9b5d29ef52b1490a5f1d3d1aebbc
Jon Redeker <jon.redeker@gmail.com>
2015-12-10 11:00:22 -0500

    chapter 1 finished

---
+++ exercise-01-06/dives.js
@@ -0,0 +1,19 @@
+var dives = [
+	{
+		site: 'Abu Gotta Ramada',
+		location: 'Hurghada, Egypt',
+		depth: 72,
+		time: 54
+	},
+	{
+		site: 'Ponte Mahoon',
+		location: 'Maehbourgh, Mauritius',
+		depth: 54,
+		time: 38
+	},
+	{
+		site: 'Molnar Cave',
+		location: 'Budapest, Hungary',
+		depth: 98,
+		time: 62
+	}];
